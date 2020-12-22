0607e4581b29bed587f95016ad6178cac7613a07
Jelena Feliciano <jfel217@gmail.com>
2020-12-22 00:23:59 -0500

    js updates

--- js/Player.js
+++ js/Player.js
@@ -1,9 +1,23 @@
 class Players {
-    constructor (name, color) {
+    constructor (name, id, color, active = false) {
         this.name = name;
+        this.id = id;
         this.color = color; 
+        this.active = active;
+        this.token = this.createToken(21);
     }
-    set() {
+    /**
+ * Creates token objects for player
+ * @param     {number}    num - Number of token objects to be created
+ * @returns   {Array}     An array of the newly created token objects
+ */
+    createToken(num) {
+        const tokens  = [];
 
+        for(let i = 0; i < num; i++) {
+            let token = new Token(i, this);
+            token.push(token);
+        }
+        return tokens;
     }
 }
--- js/Token.js
+++ js/Token.js
@@ -1,7 +1,9 @@
 //object behavior tokens move right, left, down
 class Token {
-    constructor() {
-        
+    constructor(index, owner) {
+        this.owner = owner;
+        this.id = `token-${index}-${owner.id}`;
+        this.dropped = false;
     }
    
 
