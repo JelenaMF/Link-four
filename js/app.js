ea50d93e1628cc213382d20b4d0beb7eebd705ea
Jelena Feliciano <jfel217@gmail.com>
2020-12-24 00:31:06 -0500

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const game = new Game();
+
+document.getElementById('begin-game').addEventListener('click', (e) => {
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
