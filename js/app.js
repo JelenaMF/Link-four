c846b09d8883fbb46b815e4ea4af27a68e5324f1
Jelena Feliciano <jfel217@gmail.com>
2020-12-28 00:03:28 -0500

    updated index and app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const game = new Game();
+//const game = new Game();
 
 document.getElementById('begin-game').addEventListener('click', (e) => {
-    game.startGame();
+    Game.startGame();
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
     console.log('start button works');
